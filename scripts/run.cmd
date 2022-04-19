f8fbde126af4f9d0ec179a616b79742fa4af43e3
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2022-04-19 15:51:31 -0300

    fix: run.cmd

--- scripts/run.cmd
+++ scripts/run.cmd
@@ -1,2 +1,1 @@
-javac -cp ".;./lib/gluegen-rt-natives-windows-amd64.jar;./lib/gluegen-rt.jar;./lib/jogl-all-natives-windows-amd64.jar;./lib/jogl-all.jar" christian_willian/JGLU.java \
-  && java -cp ".;./lib/gluegen-rt-natives-windows-amd64.jar;./lib/gluegen-rt.jar;./lib/jogl-all-natives-windows-amd64.jar;./lib/jogl-all.jar" christian_willian/JGLU
+javac -cp ".;./lib/gluegen-rt-natives-windows-amd64.jar;./lib/gluegen-rt.jar;./lib/jogl-all-natives-windows-amd64.jar;./lib/jogl-all.jar" christian_willian/JTestGLU.java && java -cp ".;./lib/gluegen-rt-natives-windows-amd64.jar;./lib/gluegen-rt.jar;./lib/jogl-all-natives-windows-amd64.jar;./lib/jogl-all.jar" christian_willian/JTestGLU
